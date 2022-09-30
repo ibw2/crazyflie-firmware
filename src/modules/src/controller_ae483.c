8f3e1eb021d787df0a09640ccb12a7e9a959e799
ibw2 <ibw2@illinois.edu>
2022-09-29 22:06:05 -0500

    lab 05

--- src/modules/src/controller_ae483.c
+++ src/modules/src/controller_ae483.c
@@ -18,6 +18,36 @@ static float flow_dpixely = 0.0f;
 // An example parameter
 static bool use_observer = false;
 
+// State
+static float o_x = 0.0f;
+static float o_y = 0.0f;
+static float o_z = 0.0f;
+static float psi = 0.0f;
+static float theta = 0.0f;
+static float phi = 0.0f;
+static float v_x = 0.0f;
+static float v_y = 0.0f;
+static float v_z = 0.0f;
+static float w_x = 0.0f;
+static float w_y = 0.0f;
+static float w_z = 0.0f;
+
+// Setpoint
+static float o_x_des = 0.0f;
+static float o_y_des = 0.0f;
+static float o_z_des = 0.3f;
+
+// Input
+static float tau_x = 0.0f;
+static float tau_y = 0.0f;
+static float tau_z = 0.0f;
+static float f_z = 0.0f;
+
+// Motor power command
+static uint16_t m_1 = 0;
+static uint16_t m_2 = 0;
+static uint16_t m_3 = 0;
+static uint16_t m_4 = 0;
 
 void ae483UpdateWithTOF(tofMeasurement_t *tof)
 {
@@ -105,17 +135,53 @@ void controllerAE483(control_t *control,
                      const uint32_t tick)
 {
   if (RATE_DO_EXECUTE(ATTITUDE_RATE, tick)) {
-    // Whatever is in here executes at 500 Hz
+    // Everything in here runs at 500 Hz
 
+    // Parse state (making sure to convert linear velocity from the world frame to the body frame)
+    o_x = state->position.x;
+    o_y = state->position.y;
+    o_z = state->position.z;
+    psi = radians(state->attitude.yaw);
+    theta = - radians(state->attitude.pitch);
+    phi = radians(state->attitude.roll);
+    w_x = radians(sensors->gyro.x);
+    w_y = radians(sensors->gyro.y);
+    w_z = radians(sensors->gyro.z);
+    v_x = state->velocity.x*cosf(psi)*cosf(theta) + state->velocity.y*sinf(psi)*cosf(theta) - state->velocity.z*sinf(theta);
+    v_y = state->velocity.x*(sinf(phi)*sinf(theta)*cosf(psi) - sinf(psi)*cosf(phi)) + state->velocity.y*(sinf(phi)*sinf(psi)*sinf(theta) + cosf(phi)*cosf(psi)) + state->velocity.z*sinf(phi)*cosf(theta);
+    v_z = state->velocity.x*(sinf(phi)*sinf(psi) + sinf(theta)*cosf(phi)*cosf(psi)) + state->velocity.y*(-sinf(phi)*cosf(psi) + sinf(psi)*sinf(theta)*cosf(phi)) + state->velocity.z*cosf(phi)*cosf(theta);
+
+    // Parse setpoint
+    o_x_des = setpoint->position.x;
+    o_y_des = setpoint->position.y;
+    o_z_des = setpoint->position.z;
+
+    if (setpoint->mode.z == modeDisable) {
+      // If there is no desired position, then all
+      // motor power commands should be zero
+
+      powerSet(0, 0, 0, 0);
+    } else {
+      // Otherwise, motor power commands should be
+      // chosen by the controller
+
+      // FIXME
+      tau_x = 0.00066300f * (o_y - o_y_des) -0.00439973f * phi + 0.00101699f * v_y -0.00076008f * w_x;
+      tau_y = -0.00066300f * (o_x - o_x_des) -0.00440388f * theta -0.00101727f * v_x -0.00076190f * w_y;
+      tau_z = -0.00002057f * psi -0.00002912f * w_z;
+      f_z = -0.29650261f * (o_z - o_z_des) -0.16520637f * v_z + 0.30607200f;
+
+      // FIXME
+      m_1 = limitUint16( -3770739.1f * tau_x -3770739.1f * tau_y -39494470.8f * tau_z + 122549.0f * f_z );
+      m_2 = limitUint16( -3770739.1f * tau_x + 3770739.1f * tau_y + 39494470.8f * tau_z + 122549.0f * f_z );
+      m_3 = limitUint16( 3770739.1f * tau_x + 3770739.1f * tau_y -39494470.8f * tau_z + 122549.0f * f_z );
+      m_4 = limitUint16( 3770739.1f * tau_x -3770739.1f * tau_y + 39494470.8f * tau_z + 122549.0f * f_z );
+
+      
+      // Apply motor power commands
+      powerSet(m_1, m_2, m_3, m_4);
+    }
   }
-
-  if (RATE_DO_EXECUTE(POSITION_RATE, tick)) {
-    // Whatever is in here executes at 100 Hz
-
-  }
-
-  // By default, we do nothing (set all motor power commands to zero)
-  powerSet(0, 0, 0, 0);
 }
 
 //              1234567890123456789012345678 <-- max total length
@@ -123,6 +189,29 @@ void controllerAE483(control_t *control,
 LOG_GROUP_START(ae483log)
 LOG_ADD(LOG_UINT16,         num_tof,                &tof_count)
 LOG_ADD(LOG_UINT16,         num_flow,               &flow_count)
+LOG_ADD(LOG_FLOAT,          o_x,                    &o_x)
+LOG_ADD(LOG_FLOAT,          o_y,                    &o_y)
+LOG_ADD(LOG_FLOAT,          o_z,                    &o_z)
+LOG_ADD(LOG_FLOAT,          psi,                    &psi)
+LOG_ADD(LOG_FLOAT,          theta,                  &theta)
+LOG_ADD(LOG_FLOAT,          phi,                    &phi)
+LOG_ADD(LOG_FLOAT,          v_x,                    &v_x)
+LOG_ADD(LOG_FLOAT,          v_y,                    &v_y)
+LOG_ADD(LOG_FLOAT,          v_z,                    &v_z)
+LOG_ADD(LOG_FLOAT,          w_x,                    &w_x)
+LOG_ADD(LOG_FLOAT,          w_y,                    &w_y)
+LOG_ADD(LOG_FLOAT,          w_z,                    &w_z)
+LOG_ADD(LOG_FLOAT,          o_x_des,                &o_x_des)
+LOG_ADD(LOG_FLOAT,          o_y_des,                &o_y_des)
+LOG_ADD(LOG_FLOAT,          o_z_des,                &o_z_des)
+LOG_ADD(LOG_FLOAT,          tau_x,                  &tau_x)
+LOG_ADD(LOG_FLOAT,          tau_y,                  &tau_y)
+LOG_ADD(LOG_FLOAT,          tau_z,                  &tau_z)
+LOG_ADD(LOG_FLOAT,          f_z,                    &f_z)
+LOG_ADD(LOG_UINT16,         m_1,                    &m_1)
+LOG_ADD(LOG_UINT16,         m_2,                    &m_2)
+LOG_ADD(LOG_UINT16,         m_3,                    &m_3)
+LOG_ADD(LOG_UINT16,         m_4,                    &m_4)
 LOG_GROUP_STOP(ae483log)
 
 //                1234567890123456789012345678 <-- max total length
